7f8e802b5507ea30cc6609bf37ad46a2953a341a
raulsansan <32744651+raulsansan@users.noreply.github.com>
2017-11-05 22:32:07 -0200

    Add files via upload

---
+++ e26.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+session_start();
+
+$con=mysql_connect("localhost","root","");
+mysql_select_db("mapa",$con);
+
+mysql_query("UPDATE materias set e26='1' WHERE matricula='$_SESSION[username]'",$con);
+
+header("Location: mapa.php");
+?>
